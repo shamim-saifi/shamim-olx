39fa02237ca0430c52ca4eb50901454f6430282a
shamimraees <shamimraees007@gmail.com>
2023-08-12 10:53:33 +0530

    add font

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './Components/Footer/Footer';
 import Nav from './Components/NavBar/Nav';
 import Home from './Components/Home/Home';
+import './App.css'
 
 function App() {
   return (
